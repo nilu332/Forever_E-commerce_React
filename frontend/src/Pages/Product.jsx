2151b7521cca4abadc90bd48b3ffbeaba7b87469
nilu332 <niloferp332@gmail.com>
2025-09-17 13:56:16 +0530

    updated

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,13 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
   const {productId} = useParams();
-  const {products} = useContext(ShopContext);
-  const [productData,setProductData] = useState(false);
-  const [image,setImage] = useState('')
+  const {products, currency, addToCart} = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('')
+  const [size, setSize] = useState('')
 
   const fetchProductData = async () => {
 
@@ -19,7 +22,7 @@ const Product = () => {
       }
     })
   }
-
+                                                                                                                                             
   useEffect(()=>{
     fetchProductData();
   },[productId])
@@ -41,14 +44,54 @@ const Product = () => {
               <img className='w-full h-auto' src={image} alt="" />
           </div>
         </div>
+        {/* ----------Product Info-------- */}
+        <div className='flex-1'>
+            <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+            <div className='flex items-center gap-1 mt-2'>
+              <img src={assets.star_icon} alt="" className='w-3 5'/>
+              <img src={assets.star_icon} alt="" className='w-3 5'/>
+              <img src={assets.star_icon} alt="" className='w-3 5'/>
+              <img src={assets.star_icon} alt="" className='w-3 5'/>
+              <img src={assets.star_dull_icon} alt="" className='w-3 5'/>
+              <p className='pl-2'>(122)</p>
+            </div>
+            <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+            <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+            <div className='flex flex-col gap-4 my-8'>
+              <p>Select Size</p>
+              <div className='flex gap-2'>
+                {productData.sizes.map((item,index)=>(
+                  <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                ))}
+              </div>
+            </div>
+            <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+            <hr className='mt-8 sm:w-4/5'/>
+            <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                <p>100% Original Product</p>
+                <p>Cash on Dellivery is available on this product.</p>
+                <p>Easy return and exchange policy within 7 days</p>
+            </div>
+        </div>
+      </div> 
+      {/* ---------- Description & Review Section--------------- */}
 
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm textt-gray-500'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ipsam illo nisi quidem architecto voluptatibus provident enim nostrum eum! Quibusdam quia perspiciatis architecto iste sequi atque facere autem beatae ut.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sint beatae soluta eius, numquam sunt libero quae a debitis sit earum laboriosam? At sit aliquam sed est temporibus consequuntur labore.</p>
+        </div>
       </div>
-      
+      {/* ---------- display related products ---------- */}
+
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
     </div>
   ) : <div className='opacity-0'></div>
 }
 
 export default Product                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
      
-
-
