7b1452da6675ffe7437e37c8512ee4e4bb35e6f2
nilu332 <niloferp332@gmail.com>
2025-09-20 13:46:52 +0530

    updated

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -3,9 +3,10 @@ import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import Product from './Product';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
-  const { products, currency, cartItems } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
 
@@ -33,22 +34,15 @@ const Cart = () => {
       <div>
         {cartData.map((item, index) => {
           const productData = products.find(
-            (product) => product._id === item._id // ✅ fixed lookup
+            (product) => product._id === item._id 
           );
 
           if (!productData) return null; // safety check
 
           return (
-            <div
-              key={index}
-              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
-            >
+            <div key={index} className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
               <div className="flex items-start gap-6">
-                <img
-                  className="w-16 sm:w-20"
-                  src={productData.image[0]}
-                  alt={productData.name}
-                />
+                <img className="w-16 sm:w-20" src={productData.image[0]} alt={productData.name}/>
                 <div>
                   <p className="text-xs sm:text-lg font-medium">
                     {productData.name}
@@ -61,15 +55,23 @@ const Cart = () => {
                     <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">
                       {item.size}
                     </p>
-                  </div>
-                </div>
+                  </div>                                                                    
+                </div>                                                                                       
               </div>
-              <input
-                className="border max-w-10 sm:max-w-16 px-1 sm:px-2 py-0 mt-4" type="number" min={1} defaultValue={item.quantity}/>
-                <img className='w-4 mr-4 sm:w-5 cursor-pointer mt-3' src={assets.bin_icon} alt="" />
+              <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id,item.size, Number(e.target.value))} className="border max-w-10 sm:max-w-16 px-1 sm:px-2 py-0 mt-4" type="number" min={1} defaultValue={item.quantity}/>
+              <img onClick={()=>updateQuantity(item._id,item.size,0)} className='w-4 mr-4 sm:w-5 cursor-pointer mt-3' src={assets.bin_icon} alt="" />
             </div>
           );
-        })}
+        })                                                                                                                                                                                                                                                  
+        }
+      </div>
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal/>
+          <div className='w-full text-end'>
+            <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button> 
+          </div>                                                      
+        </div>
       </div>
     </div>
   );
